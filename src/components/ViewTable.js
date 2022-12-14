64b744a70be2beadde0b99cda7fad53940a57d21
Ziad Tarek <ziadtarekfa@gmail.com>
2022-12-14 15:17:57 +0200

    refactored table into a component

---
+++ src/components/ViewTable.js
@@ -0,0 +1,57 @@
+import { AiOutlineSearch } from 'react-icons/ai';
+import { GoTrashcan } from 'react-icons/go';
+import { AiOutlineEye } from 'react-icons/ai';
+import avatar from '../assets/avatar.jpg';
+import '../componentStyles/view-table.css';
+const ViewTable = () => {
+    return (
+        <>
+            <div className='search-container'>
+                <input placeholder='Search'></input>
+                <AiOutlineSearch fill='#757575' />
+                <button>Delete User</button>
+            </div>
+
+
+            <table>
+                <thead>
+                    <tr>
+                        <th style={{ 'textAlign': 'center' }}>Avatar</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone Number</th>
+                        <th>Gender</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td style={{ 'textAlign': 'center' }}>
+                            <img src={avatar} alt='profile' className='avatar' />
+                        </td>
+                        <td >Ziad Tarek Farouk</td>
+                        <td >ziadtarekfa@gmail.com</td>
+                        <td >+201066324579</td>
+                        <td >Male</td>
+                        <td>
+                            <AiOutlineEye size='25px' />
+                        </td>
+                        <td>
+                            <GoTrashcan size='20px' />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <div className='pagination'>
+                <button>{'<'}</button>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>4</button>
+                <button>5</button>
+                <button>{'>'}</button>
+            </div>
+        </>
+    );
+}
+
+export default ViewTable;
--- src/pages/User.js
+++ src/pages/User.js
@@ -3,12 +3,10 @@ import Sidebar from '../components/Sidebar';
 import '../pageStyles/user.css';
 
 
-import { AiOutlineSearch } from 'react-icons/ai';
-import { GoTrashcan } from 'react-icons/go';
-import { AiOutlineEye } from 'react-icons/ai';
-import avatar from '../assets/avatar.jpg';
+
 import CurrentStats from '../components/CurrentStats';
 import NewStats from '../components/NewStats';
+import ViewTable from '../components/ViewTable';
 
 const User = () => {
     return (
@@ -22,112 +20,7 @@ const User = () => {
                         <NewStats />
                     </div>
                     <p>Users</p>
-                    <div className='search-container'>
-                        <input placeholder='Search'></input>
-                        <AiOutlineSearch fill='#757575' />
-                        <button>Delete User</button>
-                    </div>
-
-
-                    <table>
-                        <thead>
-                            <tr>
-                                <th style={{ 'textAlign': 'center' }}>Avatar</th>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Phone Number</th>
-                                <th>Gender</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td style={{ 'textAlign': 'center' }}>
-                                    <img src={avatar} alt='profile' className='avatar' />
-                                </td>
-                                <td >Ziad Tarek Farouk</td>
-                                <td >ziadtarekfa@gmail.com</td>
-                                <td >+201066324579</td>
-                                <td >Male</td>
-                                <td>
-                                    <AiOutlineEye size='25px' />
-                                </td>
-                                <td>
-                                    <GoTrashcan size='20px' />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td style={{ 'textAlign': 'center' }}>
-                                    <img src={avatar} alt='profile' className='avatar' />
-                                </td>
-                                <td >Ziad Tarek Farouk</td>
-                                <td >ziadtarekfa@gmail.com</td>
-                                <td >+201066324579</td>
-                                <td >Male</td>
-                                <td>
-                                    <AiOutlineEye size='25px' />
-                                </td>
-                                <td>
-                                    <GoTrashcan size='20px' />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td style={{ 'textAlign': 'center' }}>
-                                    <img src={avatar} alt='profile' className='avatar' />
-                                </td>
-                                <td >Ziad Tarek Farouk</td>
-                                <td >ziadtarekfa@gmail.com</td>
-                                <td >+201066324579</td>
-                                <td >Male</td>
-                                <td>
-                                    <AiOutlineEye size='25px' />
-                                </td>
-                                <td>
-                                    <GoTrashcan size='20px' />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td style={{ 'textAlign': 'center' }}>
-                                    <img src={avatar} alt='profile' className='avatar' />
-                                </td>
-                                <td >Ziad Tarek Farouk</td>
-                                <td >ziadtarekfa@gmail.com</td>
-                                <td >+201066324579</td>
-                                <td >Male</td>
-                                <td>
-                                    <AiOutlineEye size='25px' />
-                                </td>
-                                <td>
-                                    <GoTrashcan size='20px' />
-                                </td>
-                            </tr>
-                            <tr>
-                                <td style={{ 'textAlign': 'center' }}>
-                                    <img src={avatar} alt='profile' className='avatar' />
-                                </td>
-                                <td >Ziad Tarek Farouk</td>
-                                <td >ziadtarekfa@gmail.com</td>
-                                <td >+201066324579</td>
-                                <td >Male</td>
-                                <td>
-                                    <AiOutlineEye size='25px' />
-                                </td>
-                                <td>
-                                    <GoTrashcan size='20px' />
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <div className='pagination'>
-                        <button>{'<'}</button>
-                        <button>1</button>
-                        <button>2</button>
-                        <button>3</button>
-                        <button>4</button>
-                        <button>5</button>
-                        <button>{'>'}</button>
-                    </div>
-
-
+                    <ViewTable />
                 </div>
             </div>
         </div>
