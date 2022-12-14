8f5eca49d09e1d5c4bc7700cf3804f415c644dd5
Ziad Tarek <ziadtarekfa@gmail.com>
2022-12-14 15:11:35 +0200

    added pagination style

--- src/pages/User.js
+++ src/pages/User.js
@@ -117,6 +117,15 @@ const User = () => {
                             </tr>
                         </tbody>
                     </table>
+                    <div className='pagination'>
+                        <button>{'<'}</button>
+                        <button>1</button>
+                        <button>2</button>
+                        <button>3</button>
+                        <button>4</button>
+                        <button>5</button>
+                        <button>{'>'}</button>
+                    </div>
 
 
                 </div>
