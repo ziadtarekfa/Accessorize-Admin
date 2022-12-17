d3c9f286dfd7430e56b67c2e8e9eb847eaefdbc4
Ziad Tarek <ziadtarekfa@gmail.com>
2022-12-17 16:08:47 +0200

    added not found 404

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 import Seller from './pages/Seller';
 import User from './pages/User';
 
@@ -13,6 +14,7 @@ function App() {
           <Route path='/' element={<Login />} />
           <Route path='/manage-users' element={<User />} />
           <Route path='/manage-sellers' element={<Seller />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,27 @@
+import '../componentStyles/not-found.css';
+import lottie from "lottie-web";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import NotFoundLogo from '../assets/animations/lottie.json';
+
+const NotFound = () => {
+    useEffect(() => {
+        const instance = lottie.loadAnimation({
+            container: document.querySelector('#lottie-container'),
+            animationData: NotFoundLogo,
+        })
+
+        return () => instance.destroy()
+    }, [])
+    let navigate = useNavigate();
+    return (
+        <div className='not-found'>
+            <div id='lottie-container' />
+            <div>
+                <button onClick={() => { navigate('/') }}>Go Back To Home</button>
+            </div>
+        </div>
+    );
+}
+
+export default NotFound;
