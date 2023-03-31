aedf3fbac576835ee646905c78f84e7b236165a6
Ziad Tarek <ziadtarekfa@gmail.com>
2023-03-31 04:51:47 +0200

    implemented getCurrentUsersNumber

--- src/pages/User.js
+++ src/pages/User.js
@@ -3,16 +3,26 @@ import Sidebar from '../components/Sidebar';
 import CurrentStats from '../components/CurrentStats';
 import NewStats from '../components/NewStats';
 import ViewTable from '../components/ViewTable';
+import { useEffect, useState } from 'react';
 
 
 const User = () => {
+    const [currentUsersCount, setCurrentUsersCount] = useState(0);
+    useEffect(() => {
+        fetch("http://localhost:8000/admin/usersCount").then((res) => {
+            return res.json();
+        }).then((data) => {
+            setCurrentUsersCount(data.usersCount.toString());
+        });
+    }, []);
+
     return (
         <div className='container'>
 
             <Sidebar />
             <div className='user-content'>
                 <div className='user-stats'>
-                    <CurrentStats text='Current Users' quantity='100' />
+                    <CurrentStats text='Current Users' quantity={currentUsersCount} />
                     <NewStats text='New Users' quantity='20' />
                 </div>
                 <p>Users</p>
