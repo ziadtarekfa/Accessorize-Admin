d80a44a6d92c8bc6a5101a45ac49e4eb4fc0ec94
Ziad Tarek <ziadtarekfa@gmail.com>
2023-04-02 02:58:20 +0200

    implemented fetch recent users count

--- src/pages/User.js
+++ src/pages/User.js
@@ -8,12 +8,19 @@ import { useEffect, useState } from 'react';
 
 const User = () => {
     const [currentUsersCount, setCurrentUsersCount] = useState(0);
+    const [recentUsersCount, setRecentUsersCount] = useState(0);
     useEffect(() => {
         fetch("http://localhost:8000/admin/usersCount").then((res) => {
             return res.json();
         }).then((data) => {
-            setCurrentUsersCount(data.usersCount.toString());
+            setCurrentUsersCount(data.usersCount);
         });
+
+        fetch("http://localhost:8000/admin/recentUsers").then((res) => {
+            return res.json();
+        }).then((data) => {
+            setRecentUsersCount(data);
+        })
     }, []);
 
     return (
@@ -23,7 +30,7 @@ const User = () => {
             <div className='user-content'>
                 <div className='user-stats'>
                     <CurrentStats text='Current Users' quantity={currentUsersCount} />
-                    <NewStats text='New Users' quantity='20' />
+                    <NewStats text='New Users' quantity={recentUsersCount} />
                 </div>
                 <p>Users</p>
                 <ViewTable />
